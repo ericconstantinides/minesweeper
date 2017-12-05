33222e8ffebbd15d0c8e9c954e04300278471958
Eric Constantinides <eric@ericconstantinides.com>
2017-12-04 17:34:32 -0800

    switch to emojis

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -4,12 +4,8 @@ class ResetButton extends Component {
   render () {
     return (
       <div className='ResetButton__container'>
-        <div className='ResetButton ResetButton--sunglasses-face'>
-          <div className='ResetButton__inner'>
-            <span className='ResetButton__left-eye' />
-            <span className='ResetButton__right-eye' />
-            <span className='ResetButton__mouth' />
-          </div>
+        <div className='ResetButton'>
+          <div className='ResetButton__inner' />
         </div>
       </div>
     )
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Square = props => {
+  // statuses: mine, swept, unswept, flag
   const mineClass = props.mine ? 'is-mine' : 'not-mine'
   const sweptClass = props.swept ? 'is-swept' : 'not-swept'
   const flagClass = props.flag ? 'is-flag' : 'not-flag'
