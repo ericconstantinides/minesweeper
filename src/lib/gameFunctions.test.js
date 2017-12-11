4a9f03d00174def0935a6fa0d17d4c8e659b2b5e
Eric Constantinides <eric@ericconstantinides.com>
2017-12-11 14:00:56 -0800

    Update gameFn tests

--- src/lib/gameFunctions.test.js
+++ src/lib/gameFunctions.test.js
@@ -28,8 +28,44 @@ describe('game functions', () => {
     expect(isUnique).toBe(true)
   })
 
-  it('should lay mines', () => {
-    const board = gameFn.layMines(8, 8, TEST_MINES)
-    expect(board[8].length).toBe(9)
+  describe('when laying mines', () => {
+    let board
+    const xMax = 9
+    const yMax = 9
+    const TEST_MINES = [
+      { x: 1, y: 7 },
+      { x: 9, y: 9 },
+      { x: 5, y: 7 },
+      { x: 2, y: 2 },
+      { x: 5, y: 5 },
+      { x: 9, y: 8 },
+      { x: 1, y: 4 },
+      { x: 3, y: 1 },
+      { x: 4, y: 7 },
+      { x: 2, y: 8 }
+    ]
+
+    beforeEach(() => {
+      board = gameFn.layMines(xMax, yMax, TEST_MINES)
+    })
+
+    it('should have the correct x length', () => {
+      expect(board.length).toBe(xMax + 1)
+    })
+
+    it('should have the correct y length', () => {
+      expect(board[0].length).toBe(yMax + 1)
+    })
+
+    it('should have laid mines on the board', () => {
+      let mines = 0
+      for (let x = 0; x <= xMax; x++) {
+        for (let y = 0; y <= yMax; y++) {
+          // now see if it's swept:
+          if (board[x][y].isMine) mines++
+        }
+      }
+      expect(mines).toEqual(TEST_MINES.length)
+    })
   })
 })
